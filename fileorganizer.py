5a790b65f84b13e8c90d36f37fcacd1d25917c06
lucky <lmnarasimha2301@gmail.com.com>
2020-09-27 07:29:31 +0530

    modified

--- fileorganizer.py
+++ fileorganizer.py
@@ -1,58 +1,98 @@
+"""python project for organizing files on a particular directory by file type,
+file size, date modified """
+# import sys
 import os
 from pathlib import Path
 
 DIRECTORIES = {
     "HTML": [".html5", ".html", ".htm", ".xhtml"],
+    "MARKUP": [".md"],
     "IMAGES": [".jpeg", ".jpg", ".tiff", ".gif", ".bmp", ".png", ".bpg", "svg",
                ".heif", ".psd"],
     "VIDEOS": [".avi", ".flv", ".wmv", ".mov", ".mp4", ".webm", ".vob", ".mng",
-               ".qt", ".mpg", ".mpeg", ".3gp"],
+               ".qt", ".mpg", ".mpeg", ".3gp", ".mkv"],
     "DOCUMENTS": [".oxps", ".epub", ".pages", ".docx", ".doc", ".fdf", ".ods",
                   ".odt", ".pwi", ".xsn", ".xps", ".dotx", ".docm", ".dox",
                   ".rvg", ".rtf", ".rtfd", ".wpd", ".xls", ".xlsx", ".ppt",
-                  "pptx"],
-    "ARCHIVES": [".a", ".ar", ".cpio", ".iso", ".tar", ".gz", ".rz", ".7z",
-                 ".dmg", ".rar", ".xar", ".zip"],
+                  "pptx", ".md", ".pages", ".numbers"],
+    "ARCHIVES": [".a", ".ar", ".arh", ".tar", ".tar.bz2", ".tar.gz", ".cpio",
+                 ".tar", ".gz", ".rz", ".7z", ".rar", ".xar", ".zip", ".xz",
+                 ".pkg", ".deb", ".rpm"],
+    "DISKIMAGE": [".iso", ".img", ".vcd", ".dmg"],
     "AUDIO": [".aac", ".aa", ".aac", ".dvf", ".m4a", ".m4b", ".m4p", ".mp3",
               ".msv", "ogg", "oga", ".raw", ".vox", ".wav", ".wma"],
-    "PLAINTEXT": [".txt", ".in", ".out"],
+    "PLAINTEXT": [".txt", ".in", ".out", ".csv", ".log"],
+    "POWERSHELL": [".ps1", ".psm1", ".psd1"],
     "PDF": [".pdf"],
-    "PYTHON": [".py"],
-    "XML": [".xml"],
-    "EXE": [".exe"],
-    "SHELL": [".sh"]
+    "PYTHON": [".py", ".pyi", ".pyc"],
+    "EXECUTABLE": [".exe", ".run"],
+    "SHELL": [".sh"],
+    "DATABASE": [".db", ".sql"],
+    "C#": [".cs"],
+    "C++": [".cpp"],
+    "C": [".c"],
+    "GO": [".go"],
+    "YAML": [".yaml"],
+    "JSON": [".json"],
+    "ASP Classic": [".asp"],
+    "ASP_NET": [".aspx", ".axd", ".asx", ".asmx", ".ashx"],
+    "CSS": [".css"],
+    "Coldfusion": [".cfm"],
+    "Erlang": [".yaws"],
+    "Flash": [".swf"],
+    "Java": [".jar", ".java", ".jsp", ".jspx", ".wss", ".do", ".action"],
+    "Kotlin": [".kt", ".kts", ".ktm"],
+    "JavaScript": [".js"],
+    "TypeScript": [".ts"],
+    "Rust": [".rs", ".rlib"],
+    "Toml": [".toml"],
+    "Travis": [".travis"],
+    "Perl": [".pl"],
+    "PHP": [".php", ".php4", ".php3", ".phtml"],
+    "Ruby": [".rb", ".rhtml"],
+    "SSI": [".shtml"],
+    "XML": [".xml", ".fxml", ".rss", ".svg"],
+    "APPS": [".app", ".ipa", ".apk"],
+    "LINKS": [".webloc", ".lnk"]
 
 }
-
 FILE_FORMATS = {file_format: directory
                 for directory, file_formats in DIRECTORIES.items()
                 for file_format in file_formats}
 
 
-def organize_junk():
-    for entry in os.scandir():
-        if entry.is_dir():
-            continue
-            # des = os.getcwd()
-            # des1 = os.path.join(des, entry)
-            # os.chdir(des1)
-            # organize_junk()
-        file_path = Path(entry)
-        file_format = file_path.suffix.lower()
-        if file_format in FILE_FORMATS:
-            directory_path = Path(FILE_FORMATS[file_format])
-            directory_path.mkdir(exist_ok=True)
-            file_path.rename(directory_path.joinpath(file_path))
+class Organize:
+    def __init__(self, folder=os.getcwd(), arrangetype="e"):
+        self.folder = folder
+        self.arrangetype = arrangetype
 
-        for dirc in os.scandir():
+    def organize_junk(self):
+        for entry in os.scandir():
+            if entry.is_dir():
+                continue
+            file_path = Path(entry)
+            file_format = file_path.suffix.lower()
+            if file_format in FILE_FORMATS:
+                directory_path = Path(FILE_FORMATS[file_format])
+                directory_path.mkdir(exist_ok=True)
+                file_path.rename(directory_path.joinpath(file_path))
+
+        try:
+            os.mkdir("OTHER-FILES")
+        except:
+            pass
+
+        for dir in os.scandir():
             try:
-                os.rmdir(dirc)
+                if dir.is_dir():
+                    os.rmdir(dir)
+                else:
+                    os.rename(os.getcwd() + '/' + str(Path(dir)), os.getcwd() +
+                              '/OTHER-FILES/' + str(Path(dir)))
+
             except:
                 pass
 
 
 if __name__ == "__main__":
-    # _input = input()
-    # # des = 'c:/Users/Lakshmi Narasimha/Desktop/Coding Challenges'
-    # os.chdir(_input)
-    organize_junk()
+    Organize().organize_junk()
